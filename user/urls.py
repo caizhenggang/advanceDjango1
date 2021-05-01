b37281cb98517eceaa6982e7bafafee32895456a
蔡正刚 <caizhenggang@example.com>
2021-05-01 18:26:13 +0800

    'request和response请求和session和cookie的使用'

---
+++ user/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, re_path
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('regist/', views.regist, name='regist1'),
+    path('regist/<user_id>/', views.regist, name='regist2'),
+    path('cookie/', views.add_cookie),
+    path('del_cookie/', views.del_cookie),
+    path('login/', views.login),
+    path('code/', views.new_code),
+    path('logout/', views.logout),
+    path('list/', views.list),
+]
